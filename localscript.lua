1e259ae522c7e604ae747886cc2e18565fccd9cc
AirplaneGoBrr <71111395+AirplanegoBrr@users.noreply.github.com>
2022-02-02 10:28:50 -0800

    Create localscript.lua

---
+++ localscript.lua
@@ -0,0 +1,5 @@
+script.Parent.MouseButton1Click:Connect(function()
+    if script.Parent.Parent.TextBox.Text == "Password here" then
+        script.parent.Parent.Parent.Frame2.Visible = true
+    end
+end)
